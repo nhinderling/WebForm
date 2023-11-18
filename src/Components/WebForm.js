d9ba0b49f57f3274bc74e4a3fc719072dc5959ce
nhinderling <114612462+nhinderling@users.noreply.github.com>
2023-11-18 14:55:13 +0100

    clean up

--- src/Components/WebForm.js
+++ src/Components/WebForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './WebForm.css';
 import './Confirmation.css';
+import './BaseLayout.css'
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
@@ -92,89 +93,97 @@ function WebForm() {
 
   if (isSubmitted) {
     return (
-      <div className="submission-details">
-        <h4>We have received your request!</h4>
-        <p>Thank you, {submittedData.firstName} {submittedData.lastName}, for your interest.</p>
-        <p> We will get back to you shortly to confirm your booking.</p>
-       
-       <div className="DetailsGroup">
-        <h6>Your Booking details</h6>
-          <div className="details">
-            <p>Email: {submittedData.email}</p>
-          </div>
-          <div className="details">
-          <p>Number of People: {submittedData.numberOfPeople}</p>
-          </div>
-          <div className="details">
-          <p>Date: {submittedData.date.toString()}</p>
-          </div>
-          <div className="details">
-          <p>Time: {submittedData.time}</p>
-          </div>
-          <div className="details">
-          <p>Comments: {submittedData.comments}</p>
+      <div className="form">
+        <div className="header">
+          <h4>We have received your request!</h4>
+          <p>Thank you, {submittedData.firstName} {submittedData.lastName}, for your interest.</p>
+          <p> We will get back to you shortly to confirm your booking.</p>
+       </div>
+       <div class="CenterForm">
+        <div className="DetailsGroup">
+          <h6>Your Booking details</h6>
+            <div className="details">
+              <p>Email: {submittedData.email}</p>
+            </div>
+            <div className="details">
+            <p>Number of People: {submittedData.numberOfPeople}</p>
+            </div>
+            <div className="details">
+            <p>Date: {submittedData.date.toString()}</p>
+            </div>
+            <div className="details">
+            <p>Time: {submittedData.time}</p>
+            </div>
+            <div className="details">
+            <p>Comments: {submittedData.comments}</p>
+            </div>
           </div>
         </div>
-        <button className="submit-button" onClick={ResetForm}>Make Another Reservation</button>
+        <div className="button-container">
+         <button className="submit-button" onClick={ResetForm}>Make Another Reservation</button>
+        </div>
       </div>
     );
   }
 
   return (
-    <form onSubmit={handleSubmit} className="reservation-form">
+    <form onSubmit={handleSubmit} className="form">
       <div className="header">
         <h1>Laser Tag Super Fun</h1>
         <p>You can book your session here!</p>
       </div>
-      
-      <div className="form-row">
-        <div className="form-group">
-          <label>First Name<span class="required">*</span></label>
-          <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+      <div class="CenterForm">
+        <div className="allInput">
+          <div className="form-row">
+            <div className="form-group">
+              <label>First Name<span class="required">*</span></label>
+              <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} required />
+            </div>
+            <div className="form-group">
+              <label>Last Name <span class="required">*</span></label>
+              <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label>Email Address<span class="required">*</span></label>
+            <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+          </div>
+
+          <div className="form-group">
+            <label>Number of People<span class="required">*</span></label>
+            <input type="number" name="numberOfPeople" value={formData.numberOfPeople} onChange={handleChange} required />
+          </div>
+
+          <div className="form-row">
+            <div className="form-group">
+              <label>Date<span class="required">*</span></label>
+              <DatePicker
+                selected={formData.date}
+                onChange={handleDateChange}
+                filterDate={date => !notAvailableDate(date)}
+                minDate={new Date()}
+                dateFormat="yyyy-MM-dd"
+              />
+            </div>
+            <div className="form-group ">
+              <label>Time <span class="required">*</span></label>
+              <select name="time" value={formData.time} onChange={handleChange} required>
+                <option value="">Select a time</option>
+                {generateTimeOptions()}
+              </select>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label>Additional Comments</label>
+            <textarea name="comments" value={formData.comments} onChange={handleChange} rows="4"></textarea>
+          </div>
+        
+          <div className="button-container">
+            <button type="submit" className="submit-button">Submit Reservation</button>
+          </div>
         </div>
-        <div className="form-group">
-          <label>Last Name <span class="required">*</span></label>
-          <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} required />
-        </div>
-      </div>
-
-      <div className="form-group">
-        <label>Email Address<span class="required">*</span></label>
-        <input type="email" name="email" value={formData.email} onChange={handleChange} required />
-      </div>
-
-      <div className="form-group">
-        <label>Number of People<span class="required">*</span></label>
-        <input type="number" name="numberOfPeople" value={formData.numberOfPeople} onChange={handleChange} required />
-      </div>
-
-      <div className="form-row">
-        <div className="form-group">
-          <label>Date<span class="required">*</span></label>
-          <DatePicker
-            selected={formData.date}
-            onChange={handleDateChange}
-            filterDate={date => !notAvailableDate(date)}
-            minDate={new Date()}
-            dateFormat="yyyy-MM-dd"
-          />
-        </div>
-        <div className="form-group ">
-          <label>Time <span class="required">*</span></label>
-          <select name="time" value={formData.time} onChange={handleChange} required>
-            <option value="">Select a time</option>
-            {generateTimeOptions()}
-          </select>
-        </div>
-      </div>
-
-      <div className="form-group">
-        <label>Additional Comments</label>
-        <textarea name="comments" value={formData.comments} onChange={handleChange} rows="4"></textarea>
-      </div>
-
-      <div className="button-container">
-         <button type="submit" className="submit-button">Submit Reservation</button>
       </div>
     </form>
   );
